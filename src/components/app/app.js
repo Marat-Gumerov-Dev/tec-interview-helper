2c241339a75950ff2c93f5f8964dce4c665bfe32
Marat Gumerov <gumerovm74@gmail.com>
2021-05-08 14:07:13 +0300

    add home-page component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,28 +1,9 @@
 import React from "react";
 
-import Logo from '../header/logo';
-import MenuToggle from '../header/menu-toggle';
-import HeaderTitle from '../header/header-title';
-import ThemeSwitcher from '../header/theme-switcher';
-import SignUp from '../header/sign-up';
-import RunTest from '../run-test';
-
+import HomePage from "../home-page";
 const App = () => {
   return (
-    <div className="home-page">
-      <header>
-        <div className="menu-toggle">
-          < Logo />
-          < MenuToggle />
-        < /div>
-        < HeaderTitle />
-        < ThemeSwitcher />
-        < SignUp />
-      < /header>
-      <main>
-        < RunTest/>
-      < /main>
-    < /div>
+    <HomePage />
   )
 };
 
---
+++ src/components/home-page/home-page.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import Logo from '../header/logo';
+import MenuToggle from '../header/menu-toggle';
+import HeaderTitle from '../header/header-title';
+import ThemeSwitcher from '../header/theme-switcher';
+import SignUp from '../header/sign-up';
+import RunTest from '../run-test';
+
+
+
+const HomePage = () => {
+
+  return (
+    <div className="home-page">
+      <header>
+        <div className="logo">
+          < Logo />
+          < MenuToggle />
+        < /div>
+        < HeaderTitle />
+        < ThemeSwitcher />
+        < SignUp />
+      < /header>
+      <main>
+        < RunTest/>
+      < /main>
+    < /div>
+  )
+
+}
+
+export default HomePage;
