5bf8c55a6f35ddcfc91a8678cc3d8dbb2da20fb7
Marat Gumerov <gumerovm74@gmail.com>
2021-05-08 11:17:26 +0300

    create blocks in  index js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = <h1> Hello World</h1>;
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = () => {
+  return (
+    <div className="home-page">
+      <header>
+        <div className="menu-toggle">
+           <a href="/some/valid/uri">
+            <img src="logo" width="32px" height="32px" alt="logo"/>
+           < /a>
+          <div class="menu">
+            <ul>
+              <li><a href="/some/valid/uri">Главная< /a>< /li>
+              <li><a href="/some/valid/uri">Быстрый тест< /a>< /li>
+              <li><a href="/some/valid/uri">Настройки< /a>< /li>
+              <li><a href="/some/valid/uri">Статистика< /a>< /li>
+              <li><a href="/some/valid/uri">Автотест< /a>< /li>
+              <li><a href="/some/valid/uri">Тест с интервьюером< /a>< /li>
+              <li><a href="/some/valid/uri">Результаты теста< /a>< /li>
+            </ul>
+          < /div>
+          <h1> Tec interview helper < /h1>
+        < /div>
+        <div>
+          <span>Theme switcher< /span>
+        < /div>
+        <div>
+          <span>Sign-Up< /span>
+        < /div>
+      < /header>
+      <main>
+        <div>
+          RUN TEST
+        < /div>
+      < /main>
+    < /div>
+   
+  )
+  
+  
+}
+  
+ReactDOM.render(< App />, document.getElementById("root"));
